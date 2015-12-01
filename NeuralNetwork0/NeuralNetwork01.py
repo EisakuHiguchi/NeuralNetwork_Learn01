db3eea5ec447868b38add7c860797ad889922c86
EisakuHiguchi <13ec083@ms.dendai.ac.jp>
2015-12-01 23:22:20 +0900

    最初のコミット、ちゃんと学習できているかは確認中、学習データはMNISTを使用

---
+++ NeuralNetwork0/NeuralNetwork01.py
@@ -0,0 +1,157 @@
+﻿
+import math
+import struct
+import random
+
+class Node:
+	state = ""
+	def __init__(self,id, **kwargs):
+		self.id = id
+		self.wire = []
+		self.value = 0
+		self.bias = 0
+		return super().__init__(**kwargs)
+
+	def update(self):
+		for w in self.wire:
+			if w.nodes[1] == self:
+				w.update(activateFunc(w.w * self.value + self.bias))
+				#w.update(activateFunc(self.value))
+
+	def setWire(self, node):
+		for n in node:
+			w = Wire(self)
+			w.setNode(n)
+			self.wire.append(w)
+
+	def setWireinst(self, wire):
+		self.wire.append(wire)
+
+	def output(self, node):
+		for w in self.wire:
+			if w.nodes[1]  == node:
+				return activateFunc(w.w * self.value + self.bias)
+
+	def initInput(self, inval):
+		self.value = inval
+
+	def innerInput(self):
+		self.value = 0
+		for w in self.wire:
+			if w.nodes[1] == self:
+				self.value += w.nodes[0].output(self)
+
+	def __cmp__(self, other):
+		return self.id - other.id
+
+
+class Wire:
+	def __init__(self, node, **kwargs):
+		self.w = random.uniform(0,3)
+		self.rate = rate
+		self.nodes = [node]
+		return super().__init__(**kwargs)
+	
+	def setNode(self, node):
+		self.nodes.append(node);
+		node.setWireinst(self)
+
+	def setWeight(self, val):
+		self.w = val
+
+	def update(self, val):
+		self.w = self.w - rate * (val - output) * input
+
+
+# function
+
+def activateFunc(u):
+	try:
+		res =  1 / (1 + math.exp(u))
+	except:
+		res = 0
+	return res
+	
+def updateNode(num):
+	f = openData("t10k-images.idx3-ubyte")
+	for i in range(17):
+		f.read(28*28*(num-1))
+	for n in nodes:
+		if n.state == "input":
+			buf = f.read(1)
+			data = struct.unpack('B'*(len(buf)),buf)
+			n.initInput(data[0])
+
+	for n in nodes:
+		n.innerInput()
+	output = getLabel(num)
+	for n in nodes:
+		n.update()
+	f.close()
+
+def getLabel(num):
+	labelFile = open("t10-labels.txt");
+	for i in range(num):
+		label = labelFile.readline()
+	return int(label)
+
+def openData(filename):
+	f = open(filename,'rb')
+	f.read(16) # ヘッダ部分の読み込み
+	return f
+
+def outputprint():
+	for n in nodes:
+		if n.id > (inputNum + innerNum):
+			print(n.id, n.value)
+			#for w in n.wire:
+			#	print(" ", " ", w.w)
+
+
+# main
+
+input = 2
+outputNum = 4
+rate = 0.2
+inputRow = 28
+inputCol = 28
+layer = 3
+innerNum = 50
+inputNum = inputRow * inputCol
+output = 0
+
+nodes = []
+
+for i in range(inputNum + innerNum + outputNum):
+	n = Node(i)
+	nodes.append(n)
+
+f = openData("t10k-images.idx3-ubyte")
+
+for n in nodes:
+	if n.id < inputNum:
+		n.state = "input"
+		for i in range(innerNum):
+			n.setWire([nodes[i + inputNum]])
+
+	elif n.id < (inputNum + innerNum):
+		n.state = "inner"		
+		for i in range(outputNum):
+			n.setWire([nodes[i + inputNum + innerNum]])
+
+	else:
+		n.state = "output"
+
+f.close()
+
+#for i in nodes:
+#for j in range(inputNum):
+#	i = nodes[j]
+#	print(i.id , i.state, i.value)
+	#for w in i.wire:
+	#	print("	",w.nodes[0].id, w.nodes[1].id)
+
+for i in range(300):
+	print("loop " , i +1)
+	updateNode(1)
+	outputprint()
